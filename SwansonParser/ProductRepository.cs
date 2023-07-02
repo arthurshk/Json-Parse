972cc801f2dd5b0c65fdf7f80bec9178dfca6fe6
arthurshk <arthurdragon39@gmail.com>
2023-07-02 18:39:32 +0300

    commit added Microsoft SQL

---
+++ SwansonParser/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using SwansonParser;
+using System.Data.SqlClient;
+
+public class ProductRepository
+{
+    private readonly string _connectionString;
+
+    public ProductRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    public void InsertProduct(Product product)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            var query = "INSERT INTO Products (Number, Title, Vendor, Price) " +
+                        "VALUES (@Number, @Title, @Vendor, @Price)";
+
+            using (var command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Number", product.Number);
+                command.Parameters.AddWithValue("@Title", product.Title);
+                command.Parameters.AddWithValue("@Vendor", product.Vendor);
+                command.Parameters.AddWithValue("@Price", product.Price);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- SwansonParser/Program.cs
+++ SwansonParser/Program.cs
@@ -5,87 +5,128 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using OfficeOpenXml;
-
-namespace SwansonParser
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using SwansonParser;
+public class SwansonParser1
 {
-    public class Program
+    private readonly ProductRepository _productRepository;
+
+    public SwansonParser1(ProductRepository productRepository)
     {
-        public static void Main(string[] args)
+        _productRepository = productRepository;
+    }
+
+    public static void Main(string[] args)
+    {
+        RunSwansonParser();
+    }
+    private static void RunSwansonParser()
+    {
+        var serviceProvider = ConfigureServices();
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        var baseUrl = "https://www.swansonvitamins.com/ncat1/Vitamins+and+Supplements/ncat2/Multivitamins/ncat3/Multivitamins+with+Iron/q";
+        var pageCount = 3;
+
+        using (var scope = serviceProvider.CreateScope())
         {
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                var baseUrl = "https://www.swansonvitamins.com/ncat1/Vitamins+and+Supplements/ncat2/Multivitamins/ncat3/Multivitamins+with+Iron/q";
-                var pageCount = 3;
-                var products = ParseSwansonWebsite(baseUrl, pageCount);
-                DisplayProductData(products);
-                CreateExcelFile(products);
-                Console.WriteLine("Excel file created successfully.");
+            var parser = scope.ServiceProvider.GetRequiredService<SwansonParser1>();
+            var products = parser.ParseSwansonWebsite(baseUrl, pageCount);
+            parser.DisplayProductData(products);
+            parser.CreateExcelFile(products);
+            Console.WriteLine("Excel file created successfully.");
         }
-        public static List<Product> ParseSwansonWebsite(string baseUrl, int pageCount)
+    }
+    private static IServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddSingleton<ProductRepository>(provider => new ProductRepository(connectionString));
+        services.AddScoped<SwansonParser1>();
+
+        return services.BuildServiceProvider();
+    }
+
+    public List<Product> ParseSwansonWebsite(string baseUrl, int pageCount)
+    {
+        var allProducts = new List<Product>();
+
+        for (int i = 1; i <= pageCount; i++)
         {
-            var allProducts = new List<Product>();
+            var url = $"{baseUrl}?page={i}";
 
-            for (int i = 1; i <= pageCount; i++)
+            using (var client = new HttpClient())
             {
-                var url = $"{baseUrl}?page={i}";
+                var html = client.GetStringAsync(url).Result;
+                File.WriteAllText("page.html", html);
 
-                using (var client = new HttpClient())
+                var pattern = @"adobeRecords"":(.+),""topProduct";
+                var matches = Regex.Matches(html, pattern);
+                Console.WriteLine(matches.Count);
+
+                if (matches.Count > 0)
                 {
-                    var html = client.GetStringAsync(url).Result;
-                    File.WriteAllText("page.html", html);
+                    var json = matches[0].Groups[1].Value;
+                    var products = JsonSerializer.Deserialize<List<Product>>(json);
+                    allProducts.AddRange(products);
 
-                    var pattern = @"adobeRecords"":(.+),""topProduct";
-                    var matches = Regex.Matches(html, pattern);
-                    Console.WriteLine(matches.Count);
-
-                    if (matches.Count > 0)
+                    products.ForEach(p =>
                     {
-                        var json = matches[0].Groups[1].Value;
-                        var products = JsonSerializer.Deserialize<List<Product>>(json);
-                        allProducts.AddRange(products);
-                    }
+                        _productRepository.InsertProduct(p);
+                    });
                 }
             }
-
-            return allProducts;
         }
-        public static void DisplayProductData(List<Product> products)
+
+        return allProducts;
+    }
+
+    public void DisplayProductData(List<Product> products)
+    {
+        Console.WriteLine("Product Data:");
+        Console.WriteLine("-------------");
+
+        foreach (var product in products)
         {
-            Console.WriteLine("Product Data:");
-            Console.WriteLine("-------------");
-            foreach (var product in products)
-            {
-                Console.WriteLine($"Number: {product.Number}");
-                Console.WriteLine($"Title: {product.Title}");
-                Console.WriteLine($"Vendor: {product.Vendor}");
-                Console.WriteLine($"Price: {product.Price}");
-                Console.WriteLine();
-            }
+            Console.WriteLine($"Number: {product.Number}");
+            Console.WriteLine($"Title: {product.Title}");
+            Console.WriteLine($"Vendor: {product.Vendor}");
+            Console.WriteLine($"Price: {product.Price}");
+            Console.WriteLine();
         }
-        public static void CreateExcelFile(List<Product> products)
+    }
+
+    public void CreateExcelFile(List<Product> products)
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "products.xlsx");
+
+        using (var package = new ExcelPackage(new FileInfo(filePath)))
         {
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "products.xlsx");
+            var worksheet = package.Workbook.Worksheets.Add("Products");
+            worksheet.Cells[1, 1].Value = "Number";
+            worksheet.Cells[1, 2].Value = "Title";
+            worksheet.Cells[1, 3].Value = "Vendor";
+            worksheet.Cells[1, 4].Value = "Price";
 
-            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            for (int i = 0; i < products.Count; i++)
             {
-                var worksheet = package.Workbook.Worksheets.Add("Products");
-                worksheet.Cells[1, 1].Value = "Number";
-                worksheet.Cells[1, 2].Value = "Title";
-                worksheet.Cells[1, 3].Value = "Vendor";
-                worksheet.Cells[1, 4].Value = "Price";
+                var product = products[i];
+                var row = i + 2;
 
-                for (int i = 0; i < products.Count; i++)
-                {
-                    var product = products[i];
-                    var row = i + 2;
-
-                    worksheet.Cells[row, 1].Value = product.Number;
-                    worksheet.Cells[row, 2].Value = product.Title;
-                    worksheet.Cells[row, 3].Value = product.Vendor;
-                    worksheet.Cells[row, 4].Value = product.Price;
-                }
-                worksheet.Cells.AutoFitColumns();
-                package.Save();
+                worksheet.Cells[row, 1].Value = product.Number;
+                worksheet.Cells[row, 2].Value = product.Title;
+                worksheet.Cells[row, 3].Value = product.Vendor;
+                worksheet.Cells[row, 4].Value = product.Price;
             }
+
+            worksheet.Cells.AutoFitColumns();
+            package.Save();
         }
     }
 }
